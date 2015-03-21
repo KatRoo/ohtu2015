ab03d24bb284d4f49b3ba8c6230b45cfcb8bfa80
Katri Roos <katri.roos@cs.helsinki.fi>
2015-03-21 17:13:56 +0200

    ajan tasalle

---
+++ viikko2/Ohtu-NhlStatistics1/src/test/java/StaticsticsTest.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import ohtuesimerkki.Player;
+import ohtuesimerkki.Reader;
+import ohtuesimerkki.Statistics;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Katri Roos
+ */
+public class StaticsticsTest {
+    
+    Statistics stats;
+    Reader readerStub = new Reader() {
+        
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+                
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+   
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    @Test
+    public void hello() {}
+}
